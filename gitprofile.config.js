ecbc7f80b94d33970ddc6c61ebf9720e7232d6b2
dapamd <najelamutiara35@gmail.com>
2025-01-01 14:41:58 +0700

    dapamd

---
+++ gitprofile.config.js
@@ -0,0 +1,121 @@
+// gitprofile.config.js
+
+const config = {
+  github : {
+    username: 'dapamd', // Your GitHub org/user name. (Required)
+    sortBy: 'stars', // stars | updated
+    limit: 15, // How many projects to display.
+    exclude: {
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    },
+  },
+  social: {
+    instagram : 'dapamd.my',
+    website : 'https://dapamd.my.id',
+    phone : '+62895606423692',
+    email : 'dapamd@gmail.com',
+  },
+  resume: {
+    fileUrl:
+      'https://dapamd.my.id', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'Copas',
+    'Recode',
+    'Reupload',
+  ],
+  experiences: [
+    ],
+  /* certifications: [
+    {
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com'
+    },
+  ], */
+  education: [
+    {
+      institution: 'SMK Negeri 1 Pemalang',
+      degree: 'Student',
+      from: '2022',
+      to: '2025',
+    },
+  ],
+  // Display blog posts from your medium or dev account. (Optional)
+  blog: {
+    source: 'dev', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 3, // How many posts to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'cupcake',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'procyon',
+    ],
+
+    // Custom theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+};
+
+export default config;
